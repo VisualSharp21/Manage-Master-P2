a681beef87f1ad4fbd324fe4b2917e724c815d0e
bruno.almd <brunoalmeida22007@gmail.com>
2026-01-25 11:55:51 -0300

    UserRepository Finalizado

--- sigfebackend/src/main/java/com/sigfe/backend/repository/UserRepository.java
+++ sigfebackend/src/main/java/com/sigfe/backend/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.sigfe.backend.repository;
 
+import com.sigfe.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
